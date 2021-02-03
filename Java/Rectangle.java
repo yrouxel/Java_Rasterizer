c1b1985adb3b09b924403f1734863857f9fb7c0f
NowSayMyName <yoann.rouxel@insa-lyon.fr>
2021-02-03 22:16:53 +0100

    added missing file

---
+++ Java/Rectangle.java
@@ -0,0 +1,26 @@
+public class Rectangle {
+	private int xMin, xMax, yMin, yMax;
+
+	public Rectangle(int xMin, int xMax, int yMin, int yMax) {
+		this.xMin = xMin;
+		this.xMax = xMax;
+		this.yMin = yMin;
+		this.yMax = yMax;
+	}
+
+	public int getxMin() {
+		return xMin;
+	}
+
+	public int getxMax() {
+		return xMax;
+	}
+
+	public int getyMin() {
+		return yMin;
+	}
+
+	public int getyMax() {
+		return yMax;
+	}
+}
